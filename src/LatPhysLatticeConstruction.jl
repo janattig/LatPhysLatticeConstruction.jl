4e90ce8202a0353b1464720f668ba57ca0047fe2
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-15 14:18:22 +0100

    added the code from the main module

--- src/LatPhysLatticeConstruction.jl
+++ src/LatPhysLatticeConstruction.jl
@@ -1,5 +1,43 @@
+################################################################################
+#
+#   module LatticePhysics_LatticeConstruction
+#   -> LatticePhysics_Base
+#   -> LinearAlgebra
+#
+#   --> NAIVE CONSTRUCTION OF LATTICES
+#           - periodic pattern of unitcells
+#           - semi-periodic pattern of unitcells
+#           - open pattern of unitcells
+#           - by bond distance to an origin site
+#           - in a shape around an origin site
+#
+#   --> ADDING NNN TO LATTICE
+#
+#   --> LATTICE CONSTRUCTION ALGORITHM FROM SPIN SPIRALS PAPER
+#
+################################################################################
+
+# module start
 module LatPhysLatticeConstruction
 
-greet() = print("Hello World!")
 
-end # module
+# uses the Base package
+using LatPhysBase
+
+
+
+
+
+
+
+# FUNCTIONS TO DEFINE INDICES IN ARRAYS
+include("index_functions.jl")
+
+# CONSTRUCTION OF LATTICES FROM UNITCELLS
+# BY PUTTING TOGETHER UNITCELLS
+include("unitcell_patterns_periodic.jl")
+
+
+
+# module end
+end
