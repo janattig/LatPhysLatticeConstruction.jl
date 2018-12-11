c425496108424a2a322464441d3e4ffdb8c51eb5
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-12-11 10:17:08 +0100

    updated concanation of bonds with assertion message

--- src/neighbors_NNN_by_bonds.jl
+++ src/neighbors_NNN_by_bonds.jl
@@ -4,11 +4,11 @@
 function concanateBonds(
             b1 :: B,
             b2 :: B,
-            labelfunction :: Function
+            labelfunction :: Function = (bo1,bo2) -> getDefaultLabel(L)
         ) :: B where {L,B<:AbstractBond{L,0}}
 
     # assert that the bonds can indeed by concanated
-    @assert to(b1) == from(b2)
+    @assert to(b1) == from(b2) "Bonds cannot be concanated as "*string(from(b1))*"-"*string(to(b1))*" (-) "*string(from(b2))*"-"*string(to(b2))*" not possible"
 
     # create a new bond
     return newBond(
@@ -23,11 +23,11 @@ end
 function concanateBonds(
             b1 :: B,
             b2 :: B,
-            labelfunction :: Function
+            labelfunction :: Function = (bo1,bo2) -> getDefaultLabel(L)
         ) :: B where {L,B<:AbstractBond{L,1}}
 
     # assert that the bonds can indeed by concanated
-    @assert to(b1) == from(b2)
+    @assert to(b1) == from(b2) "Bonds cannot be concanated as "*string(from(b1))*"-"*string(to(b1))*" (-) "*string(from(b2))*"-"*string(to(b2))*" not possible"
 
     # create a new bond
     return newBond(
@@ -44,11 +44,11 @@ end
 function concanateBonds(
             b1 :: B,
             b2 :: B,
-            labelfunction :: Function
+            labelfunction :: Function = (bo1,bo2) -> getDefaultLabel(L)
         ) :: B where {L,B<:AbstractBond{L,2}}
 
     # assert that the bonds can indeed by concanated
-    @assert to(b1) == from(b2)
+    @assert to(b1) == from(b2) "Bonds cannot be concanated as "*string(from(b1))*"-"*string(to(b1))*" (-) "*string(from(b2))*"-"*string(to(b2))*" not possible"
 
     # create a new bond
     return newBond(
@@ -66,11 +66,11 @@ end
 function concanateBonds(
             b1 :: B,
             b2 :: B,
-            labelfunction :: Function
+            labelfunction :: Function = (bo1,bo2) -> getDefaultLabel(L)
         ) :: B where {L,B<:AbstractBond{L,3}}
 
     # assert that the bonds can indeed by concanated
-    @assert to(b1) == from(b2)
+    @assert to(b1) == from(b2) "Bonds cannot be concanated as "*string(from(b1))*"-"*string(to(b1))*" (-) "*string(from(b2))*"-"*string(to(b2))*" not possible"
 
     # create a new bond
     return newBond(
